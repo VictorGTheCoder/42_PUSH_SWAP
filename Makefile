b7b260b65a0a024cdd1e33daa68670676f131222
victor.giordani <victor.giordani@protonmail.com>
2022-11-21 09:50:49 +0100

    Update

---
+++ Makefile
@@ -0,0 +1,27 @@
+SRCS = utils.c sort_utils.c push_and_pop.c main.c
+
+CC = gcc
+
+FLAGS = -Werror -Wall -Wextra -fsanitize=address
+
+OBJS = ${SRCS:.c=.o}
+
+NAME = push_swap
+
+.c.o:
+			${CC} ${FLAGS} -c $< -o ${<:.c=.o}
+
+${NAME}:	${OBJS}
+			${CC} ${FLAGS} -o ${NAME} ${OBJS}
+
+all: ${NAME}
+
+clean:
+			${RM} ${OBJS}
+
+fclean:		clean
+			${RM} ${NAME}
+
+re:			fclean all
+
+.PHONY:		all clean fclean re
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: victorgiordani01 <victorgiordani01@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 22:07:53 by victorgiord       #+#    #+#             */
-/*   Updated: 2022/11/20 11:46:13 by victorgiord      ###   ########.fr       */
+/*   Updated: 2022/11/20 19:38:32 by victorgiord      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,34 @@
 
 void	sort(t_dlist stack_a, t_dlist stack_b)
 {
-	(void) stack_a;
-	(void) stack_b;
+	printf("SORT\n");
+	t_node *element;
+	int		i;
+	int		action;
+
+	action = 0;
+	i = 0;
+	while (list_length(stack_a) != 0)
+	{	
+		printf("Nombre d'action %d\n", action);
+
+		element = stack_a->first;
+		if (element->value == i)
+		{
+			push(stack_b, stack_a);
+			print_list(stack_a);
+			print_list(stack_b);
+			printf("-----------------------\n");
+			i++;
+			action++;	
+		}
+		else
+		{
+			action++;
+			rotate(stack_a);
+		}
+		
+	}
 }
 
 void	push_swap(int *entry_list)
@@ -25,26 +51,59 @@ void	push_swap(int *entry_list)
 
 	while (*entry_list != -1)
 	{
-		stack_a = push_front(stack_a, *entry_list);
+		stack_a = push_back(stack_a, *entry_list);
 		entry_list++;
 	}
-	
+	stack_b = push_back(stack_b, 0);
 	print_list(stack_a);
 	print_list(stack_b);
 
 	printf("<------------------------------------------------------------->\n");
-
-	rr(stack_a, stack_b);
+	//push(stack_b, stack_a);
+	sort(stack_a, stack_b);
+	
 	print_list(stack_a);
 	print_list(stack_b);
 
 	free_list(stack_a);
+	free_list(stack_b);
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
-	int nb[] = {1,2,31,0,4,5,6,7,8,9,-1};
+	int nb_input[] = {0,5,2,8,9,3,1,4,7,6,-1};
+	//char	*input;
+	//int		*int_input = malloc(100 *(sizeof(int)));
+	//int		i = 0;
+	printf("MAIN\n");
+	/*if (argc == 1)
+	{
+		printf("Pas de liste fourni\n");
+		return (0);
+	}
+	if (argc > 2)
+	{
+		printf("Trop d'arguments fournis\n");
+		return (0);
+	}
+	input = (char *)argv[1];
+	while (*input)
+	{
+		if (*input != ' ')
+		{
+			int_input[i] = *input - '0';
+			i++;
+		}
+		input++;
+	}
+	int_input[i] = -1;
+	i = 0;
+	while (int_input[i] != -1)
+	{
+		printf("%d-", int_input[i]);
+		i++;
+	}*/
 
-	push_swap(nb);
+	push_swap(nb_input);
 	return 0;
 }
